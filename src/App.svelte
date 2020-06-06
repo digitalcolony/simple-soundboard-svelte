1ccdf63b37a61f46ea7c43206fbf7fa955564362
Michael Allen Smith <digitalcolony@gmail.com>
2020-06-06 13:50:58 -0700

    init working

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount } from "svelte";
+  import Soundboard from "./Soundboard.svelte";
+  export let name;
+  let sounds = [];
+  let displayList = [];
+
+  onMount(async () => {
+    const res = await fetch(`soundboard.json`);
+    sounds = await res.json();
+    displayList = sounds.files;
+  });
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Soundboard</title>
+</svelte:head>
+
+<main>
+  <section class="container">
+
+    <Soundboard bind:sounds={displayList} />
+
+  </section>
+</main>
---
+++ src/Soundboard.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let sounds;
+//   let counter = 0;
+//   const increment = () => counter++;
+
+  function playSound(mp3) {
+    const sound = new Audio(mp3.mp3);
+    sound.play();
+  }
+</script>
+
+{#each sounds as { name, artist, mp3 }}
+
+  <span class="track">
+    <button class="mybutton" title={artist} on:click={() => playSound({ mp3 })}>
+      {name}
+    </button>
+  </span>
+{:else}
+  <p>no drops found</p>
+{/each}
